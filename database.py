b189620b49b153a12c567df9a26e5481247c7693
AndrewTechDeveloper <andrew.tech.developer@gmail.com>
2021-02-21 12:02:48 +0900

    use alembic

--- database.py
+++ database.py
@@ -1,16 +1,11 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-user_name = "root"
-password = "password"
-host = "db"
-database_name = "fastapi_practice_development"
+from configs import settings
 
-DATABASE = f"mysql://{user_name}:{password}@{host}/{database_name}"
+conf = settings.databases.local
+DATABASE = f"mysql://{conf.user_name}:{conf.password}@{conf.host}/{conf.database_name}"
 
 engine = create_engine(DATABASE, encoding="utf-8", echo=True)
-
-Base = declarative_base()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
